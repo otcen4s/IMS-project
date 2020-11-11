18f7352a13ddf03bd954e6a2360fb215b6aa4904
gazo.mario99@gmail.com <mariogazo@MacBook-Pro-uzivatela-Mario.local>
2020-11-11 23:29:55 +0100

    SIR

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include "main.h"
 
+using namespace std;
 
-int main(void){
+/// Globals
+long double
+         S = 9999.0,    I = 1.0, R = 0.0,
+      Sold = 9999.0, Iold = 1.0;
 
-    Init(START_TIME, END_TIME);
-    
-    Run();
+long double transmissionRate = 0.00001, recoveryRate = 0.0001;
+
+/// Next step
+void calculateSIR() {
+    S -= (Sold * Iold * transmissionRate);
+
+    I += (Sold * Iold * transmissionRate);
+    I -= (recoveryRate * Iold);
+
+    R += (recoveryRate * Iold);
+
+    Sold = S;
+    Iold = I;
+}
+
+int main(int argc, char** argv){
+    if (argc != 2) {
+        return 1;
+    }
+
+    unsigned long steps = std::strtol(argv[1], nullptr, 10);
+    /// Simulation start
+    for (unsigned long i = 0; i < steps; i++) {
+        cout << i + 1 << " S = " << round(S) << " I = " << round(I) << " R = " << round(R) << endl;
+        calculateSIR();
+    }
 
     return 0;
 }
